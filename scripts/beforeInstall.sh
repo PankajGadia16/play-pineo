cd07e501c10408c5eff8c3fafc8cb1c3b6ba67cf
Pankaj Gadia <pankajgadia1993@gmail.com>
2020-09-07 04:26:27 +0530

    changes scripts

--- scripts/beforeInstall.sh
+++ scripts/beforeInstall.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-# Install node.js
-# sudo apt-get install python-software-properties -y
-# sudo apt-add-repository ppa:chris-lea/node.js -y
-# sudo apt-get update
-# sudo apt-get install nodejs -y
-
 sudo apt-get update
 sudo apt-get install nodejs
 sudo apt-get install npm
