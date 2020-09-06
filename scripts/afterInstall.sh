5b5ed506a4ea3676127e3616daa9176e2051e69a
Pankaj Gadia <pankajgadia1993@gmail.com>
2020-09-07 01:54:06 +0530

    scripts for codeDeploy

---
+++ scripts/afterInstall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# mkdir /home/ubuntu/nodejs
+cd /home/ubuntu/play-pineo
+
+sudo npm install
---
+++ scripts/beforeInstall.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+# Install node.js
+# sudo apt-get install python-software-properties -y
+# sudo apt-add-repository ppa:chris-lea/node.js -y
+# sudo apt-get update
+# sudo apt-get install nodejs -y
+
+yum -y update
+yum -y install nodejs
+
+
+# install pm2 module globaly
+# npm install -g pm2
+# pm2 update
