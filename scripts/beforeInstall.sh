eee12c5d174a0cf5b961d2f4021f70a9668368a5
Pankaj Gadia <pankajgadia1993@gmail.com>
2020-09-08 01:23:24 +0530

    script changes

--- scripts/beforeInstall.sh
+++ scripts/beforeInstall.sh
@@ -4,5 +4,5 @@ curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y npm
 # install pm2 module globaly
-# npm install -g pm2
-# pm2 update
+npm install -g pm2
+pm2 update
