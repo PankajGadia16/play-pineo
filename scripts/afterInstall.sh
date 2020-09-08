7aac40b24eaf8a59a454e4272a22e4a29306862f
Pankaj Gadia <pankajgadia1993@gmail.com>
2020-09-08 23:02:42 +0530

    c

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /home/ubuntu/play-pineo
-rm -rf .env
-touch .env
-chmod 777 .env
+sudo rm -rf .env
+sudo touch .env
+sudo chmod 777 .env
 aws ssm get-parameters --output text --region ap-south-1 --names /env/play-pineo --with-decryption --query Parameters[0].Value > .env
 source .env
 sudo npm install
