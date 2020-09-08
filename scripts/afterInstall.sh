faf6d017dc82790fa331b809ce09a5929c316e44
Pankaj Gadia <pankajgadia1993@gmail.com>
2020-09-08 22:38:07 +0530

    changes

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-aws ssm get-parameters --output text --region ap-south-1 --names /env/play-pineo --with-decryption --query Parameters[0].Value > /home/ubuntu/play-pineo/.env
-
 cd /home/ubuntu/play-pineo
+rm -rf .env
+touch .env
+chmod 777 .env
+aws ssm get-parameters --output text --region ap-south-1 --names /env/play-pineo --with-decryption --query Parameters[0].Value > .env
 source .env
 sudo npm install
