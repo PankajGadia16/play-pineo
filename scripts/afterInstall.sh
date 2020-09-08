c849028da4f864a5b0f6ff1be268674c3bbd73e3
Pankaj Gadia <pankajgadia1993@gmail.com>
2020-09-08 22:29:47 +0530

    c

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-aws ssm get-parameters --output text --region ap-south-1 --names /env/play-pineo --with-decryption --query Parameters[0].Value > /home/ubuntu/code-deploy/.env
+aws ssm get-parameters --output text --region ap-south-1 --names /env/play-pineo --with-decryption --query Parameters[0].Value > /home/ubuntu/play-pineo/.env
 
 cd /home/ubuntu/play-pineo
 source .env
