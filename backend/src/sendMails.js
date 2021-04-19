7d2193a4d8710f0c8fd2b0a295a2fe473658c044
Pankaj Gadia <pankajgadia1993@gmail.com>
2021-04-19 14:24:39 +0530

    letters

--- backend/src/sendMails.js
+++ backend/src/sendMails.js
@@ -3,30 +3,38 @@ const wait = time => new Promise(resolve => setTimeout(resolve, time));
 
 const baseDetails = {
     from: '"Chutney ka Momo" <pankajgadia1993@gmail.com>', // sender address
-    to: "gadiap16@gmail.com", // list of receivers
+    to: "dolly.agarwal2004@gmail.com, gadiap16@gmail.com", // list of receivers  
     subject: "To the love of my life... ❤️❤️❤️"
 }
 
-const letters = [1,2,3,4,5,6]
+const letters = [12,3,4,5,6]
 
 export default async function () {
 
     let transporter = nodemailer.createTransport({
         host: "SMTP",
         service: "Gmail",
-        port: 587,
-        secure: false,
         auth: {
+            // XOAuth2: {
             user: "pankajgadia1993@gmail.com",
-            pass: "0Pankaj@g1"
+            pass: "0Pankaj@g1",
+            port: 587,
+            secure: false,
+            // user: "pankajgadia1993@gmail.com",
+            // clientId: "675038558877-urhm6togkh3b850n2jkf9ghp9rbjvvh9.apps.googleusercontent.com",
+            // clientSecret: "9Lxq8Xss8FEv49DwRHNGZQa4",
+            // refreshToken: "1//04otvyADWEAmrCgYIARAAGAQSNwF-L9Ir2VoRNht-_O8bX-1a_q56k14tZyBJcUU7vODF0lRYwviHh84p7UYLZ4Ix-cN7O7nSqiI"
+            // }
         },
 
     });
 
     for (let i of letters) {
-        const details  = { ...baseDetails, ...require(`./letters/letter${i}.js`).default }
+        if (i == 1) await wait(1000 * 60 * 60 * 21)
+        else await wait(1000 * 60 * 6 * 15)
+        const details = { ...baseDetails, ...require(`./letters/letter${i}.js`).default }
         let info = await transporter.sendMail(details);
-        wait(2000)
+
         console.log(`${Date.now()} Message sent: %s`, info.messageId);
     }
     console.log(`Done for ${letters.length} letters!`)
